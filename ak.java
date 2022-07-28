7635b485a2461150f7899bc4f957586b416a5d14
root <root@ip-172-31-1-78.us-west-1.compute.internal>
2022-07-28 16:23:09 +0000

    ak.java added

---
+++ ak.java
@@ -0,0 +1,8 @@
+hiiii
+how are you papa
+y r u left in dis world
+plzz come back to my wolrd
+am waiting for u 
+am seeing atlist once ur face
+listening atlist once ur voice i can't forget u
+baby!!
